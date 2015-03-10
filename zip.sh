46cf4e4c0dbea32be8bb44e4f0f2b3dd61998459
Kamil Nikel <knikel@posteo.de>
2015-03-10 22:25:12 +0100

    Add initial Android assets

--- zip.sh
+++ zip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rm ios.zip logos.zip web.zip
+rm ios.zip logos.zip web.zip android.zip
 zip -r -9 ios.zip iOS
 zip -r -9 logos.zip logos
 zip -r -9 web.zip web
+zip -r -9 android.zip android
