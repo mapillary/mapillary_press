bab03074a15648dc10404328f8cd234470de3e7a
Kamil Nikel <knikel@users.noreply.github.com>
2018-04-05 14:10:09 +0200

    fix: update zip script

--- zip.sh
+++ zip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-rm ios.zip logos.zip web.zip android.zip
+rm ios.zip logos.zip web.zip android.zip computer\ vision.zip
 zip -r -9 ios.zip iOS
 zip -r -9 logos.zip logos
 zip -r -9 web.zip web
 zip -r -9 android.zip android
+zip -r -9 computer\ vision.zip computer\ vision
